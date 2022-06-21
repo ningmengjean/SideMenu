07ec426047c2155754ba5a993f5316ce3ebb041e
zhuxiaojin <zhuxiaojin@eyou.net>
2022-06-21 17:33:06 +0800

    origin

---
+++ SideMenu/MailListViewController.swift
@@ -0,0 +1,88 @@
+//
+//  ViewController.swift
+//  SideMenu
+//
+//  Created by 朱晓瑾 on 2022/6/21.
+//
+
+import UIKit
+import Foundation
+
+class MailListViewController: UIViewController {
+
+    var isSlideInMenuPresented = false
+    
+    var slideInMenuPadding: CGFloat {
+        if UIDevice.current.userInterfaceIdiom == .pad {
+            return self.view.frame.width * 0.70
+        }
+        return self.view.frame.width * 0.30
+    }
+    
+    lazy var menuBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "sidebar.leading")?.withRenderingMode(.alwaysOriginal), style: .done, target: self, action: #selector(menuBarButtomItemTapped))
+
+    @objc
+    func menuBarButtomItemTapped() {
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0, options: .curveEaseInOut) { [self] in
+            self.containerView.frame.origin.x = self.isSlideInMenuPresented ? 0 : self.containerView.frame.width - self.slideInMenuPadding
+            
+        } completion: { (finished) in
+            print("Animation finished: \(finished)")
+            self.isSlideInMenuPresented.toggle()
+        }
+        
+    }
+    
+    lazy var menuView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .gray
+        return view
+    }()
+    
+    lazy var containerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(red: 255, green: 255, blue: 255, alpha: 0.3)
+        return view
+    }()
+    
+    lazy var tableView: UITableView = {
+        let view = UITableView()
+        view.frame = self.view.frame
+        view.backgroundColor = .brown
+        return view
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+        title = "Side menu"
+        navigationItem.setLeftBarButton(menuBarButtonItem, animated: false)
+        view.addSubview(tableView)
+        
+        menuView.pinMenuTo(view, with: slideInMenuPadding)
+        containerView.edgeTo(view)
+        //view.addSubview(tableView)
+    }
+}
+
+public extension UIView {
+    func edgeTo(_ view: UIView) {
+        view.addSubview(self)
+        translatesAutoresizingMaskIntoConstraints = false
+        topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }
+    
+    func pinMenuTo(_ view: UIView, with constant: CGFloat) {
+        view.addSubview(self)
+        translatesAutoresizingMaskIntoConstraints = false
+        topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -constant).isActive = true
+        bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }
+}
+
--- SideMenu/SceneDelegate.swift
+++ SideMenu/SceneDelegate.swift
@@ -16,7 +16,23 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        window?.makeKeyAndVisible()
+        let tabVC = UITabBarController()
+        let vc0 = UINavigationController(rootViewController: MailListViewController())
+        let vc1 = ViewController1()
+        vc1.tabBarItem.title = "V1"
+        let vc2 = UINavigationController(rootViewController: ViewController2())
+        vc2.tabBarItem.title = "V2"
+        tabVC.addChild(vc0)
+        tabVC.addChild(vc1)
+        tabVC.addChild(vc2)
+        
+        window?.rootViewController = tabVC
+        
+        
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
