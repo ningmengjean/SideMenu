264e8a5e6d6a46d2f2921bf29cd0478f186cefe3
zhuxiaojin <zhuxiaojin@eyou.net>
2022-06-22 16:51:46 +0800

    ipad

--- SideMenu/MailListViewController.swift
+++ SideMenu/MailListViewController.swift
@@ -25,7 +25,7 @@ class MailListViewController: UIViewController {
     func menuBarButtomItemTapped() {
         UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0, options: .curveEaseInOut) { [self] in
             self.menuView.frame.origin.x = self.isSlideInMenuPresented ? -(view.frame.width - slideInMenuPadding) : 0
-            self.containerView.isHidden = self.isSlideInMenuPresented ? true : false
+            self.maskView.isHidden = self.isSlideInMenuPresented ? true : false
             
         } completion: { (finished) in
             print("Animation finished: \(finished)")
@@ -50,9 +50,8 @@ class MailListViewController: UIViewController {
                 })
             } else {
                 UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0, options: .curveEaseInOut) { [self] in
-                    self.menuView.frame.origin.x = self.isSlideInMenuPresented ? -(view.frame.width - slideInMenuPadding) : 0
-                    self.containerView.isHidden = self.isSlideInMenuPresented ? true : false
-                    
+                    self.menuView.frame.origin.x = -(view.frame.width - slideInMenuPadding)
+                    self.maskView.isHidden = true
                 } completion: { (finished) in
                     print("Animation finished: \(finished)")
                     self.isSlideInMenuPresented.toggle()
@@ -60,6 +59,7 @@ class MailListViewController: UIViewController {
             }
         default:
             guard (t.x < 0 || self.menuView.frame.origin.x > 0 ) else { return }
+            break
         }
     }
     
@@ -67,13 +67,21 @@ class MailListViewController: UIViewController {
         let view = UIView()
         var viewTranslation = CGPoint(x: 0, y: 0)
         view.backgroundColor = .green
-        let gesture = UIPanGestureRecognizer(target: self, action: #selector(self.slipOut))
-        view.addGestureRecognizer(gesture)
-        view.isUserInteractionEnabled = true
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            let gesture = UIPanGestureRecognizer(target: self, action: #selector(self.slipOut))
+            view.addGestureRecognizer(gesture)
+            view.isUserInteractionEnabled = true
+        }
         return view
     }()
     
     lazy var containerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .red
+        return view
+    }()
+    
+    lazy var maskView: UIView = {
         let view = UIView()
         view.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.2)
         view.isOpaque = true
@@ -82,24 +90,22 @@ class MailListViewController: UIViewController {
         view.isUserInteractionEnabled = true
         return view
     }()
-    
-    lazy var tableView: UITableView = {
-        let view = UITableView()
-        view.frame = self.view.frame
-        view.backgroundColor = .yellow
-        return view
-    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .yellow
         title = "Side menu"
-        navigationItem.setLeftBarButton(menuBarButtonItem, animated: false)
-        view.addSubview(tableView)
-        containerView.edgeTo(view)
-        containerView.isHidden = true
-        menuView.pinMenuTo(view, with: (view.frame.width - slideInMenuPadding))
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            navigationItem.setLeftBarButton(menuBarButtonItem, animated: false)
+            containerView.edgeTo(view)
+            maskView.edgeTo(view)
+            maskView.isHidden = true
+            menuView.pinMenuTo(view, with: (view.frame.width - slideInMenuPadding))
+        } else {
+            containerView.ipadContainerEdgeTo(view, with: slideInMenuPadding)
+            menuView.ipadpinMenuTo(view, with: (view.frame.width - slideInMenuPadding))
+        }
     }
 }
 
@@ -120,6 +126,24 @@ extension UIView {
         topAnchor.constraint(equalTo: view.topAnchor).isActive = true
         widthAnchor.constraint(equalToConstant: constant).isActive = true
         trailingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }
+    
+    func ipadContainerEdgeTo(_ view: UIView, with constant: CGFloat) {
+        view.addSubview(self)
+        translatesAutoresizingMaskIntoConstraints = false
+        topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        widthAnchor.constraint(equalToConstant: constant).isActive = true
+        trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }
+    
+    func ipadpinMenuTo(_ view: UIView, with constant: CGFloat) {
+        view.addSubview(self)
+        translatesAutoresizingMaskIntoConstraints = false
+        topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        widthAnchor.constraint(equalToConstant: constant).isActive = true
+        leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
     }
 }
