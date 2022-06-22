ec35ece859697b6ebae225e772ccbc76d2508f91
zhuxiaojin <zhuxiaojin@eyou.net>
2022-06-22 15:35:32 +0800

    add pangesture

--- SideMenu/MailListViewController.swift
+++ SideMenu/MailListViewController.swift
@@ -14,9 +14,9 @@ class MailListViewController: UIViewController {
     
     var slideInMenuPadding: CGFloat {
         if UIDevice.current.userInterfaceIdiom == .pad {
-            return self.view.frame.width * 0.70
+            return self.view.frame.width * 0.60
         }
-        return self.view.frame.width * 0.30
+        return self.view.frame.width * 0.20
     }
     
     lazy var menuBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "sidebar.leading")?.withRenderingMode(.alwaysOriginal), style: .done, target: self, action: #selector(menuBarButtomItemTapped))
@@ -24,7 +24,8 @@ class MailListViewController: UIViewController {
     @objc
     func menuBarButtomItemTapped() {
         UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0, options: .curveEaseInOut) { [self] in
-            self.containerView.frame.origin.x = self.isSlideInMenuPresented ? 0 : self.containerView.frame.width - self.slideInMenuPadding
+            self.menuView.frame.origin.x = self.isSlideInMenuPresented ? -(view.frame.width - slideInMenuPadding) : 0
+            self.containerView.isHidden = self.isSlideInMenuPresented ? true : false
             
         } completion: { (finished) in
             print("Animation finished: \(finished)")
@@ -33,40 +34,78 @@ class MailListViewController: UIViewController {
         
     }
     
+    @objc
+    func slipOut(gestureRecognizer: UIPanGestureRecognizer) {
+        let t = gestureRecognizer.translation(in: self.menuView)
+        
+        guard (t.x < 0 || self.menuView.frame.origin.x > 0) else { return }
+        switch gestureRecognizer.state {
+        case .changed,.began:
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0, options: .curveEaseOut, animations: {
+                self.menuView.transform = CGAffineTransform(translationX: t.x, y: 0)
+            })
+        case .ended:
+            if -t.x < self.menuView.frame.width/2 {
+                UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0, options: .curveEaseOut, animations: {
+                    self.menuView.transform = .identity
+                })
+            } else {
+                UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0, options: .curveEaseInOut) { [self] in
+                    self.menuView.frame.origin.x = self.isSlideInMenuPresented ? -(view.frame.width - slideInMenuPadding) : 0
+                    self.containerView.isHidden = self.isSlideInMenuPresented ? true : false
+                    
+                } completion: { (finished) in
+                    print("Animation finished: \(finished)")
+                    self.isSlideInMenuPresented.toggle()
+                }
+            }
+        default:
+            break
+        }
+    }
+    
     lazy var menuView: UIView = {
         let view = UIView()
-        view.backgroundColor = .gray
+        var viewTranslation = CGPoint(x: 0, y: 0)
+        view.backgroundColor = .green
+        let gesture = UIPanGestureRecognizer(target: self, action: #selector(self.slipOut))
+        view.addGestureRecognizer(gesture)
+        view.isUserInteractionEnabled = true
         return view
     }()
     
     lazy var containerView: UIView = {
         let view = UIView()
-        view.backgroundColor = UIColor(white: 1, alpha: 0.3)
+        view.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.2)
+        view.isOpaque = true
+        let tap = UITapGestureRecognizer(target: self, action: #selector(self.menuBarButtomItemTapped))
+        view.addGestureRecognizer(tap)
+        view.isUserInteractionEnabled = true
         return view
     }()
     
     lazy var tableView: UITableView = {
         let view = UITableView()
         view.frame = self.view.frame
-        view.backgroundColor = .brown
+        view.backgroundColor = .yellow
         return view
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        view.backgroundColor = .red
+        view.backgroundColor = .yellow
         title = "Side menu"
         navigationItem.setLeftBarButton(menuBarButtonItem, animated: false)
         view.addSubview(tableView)
-        
-        menuView.pinMenuTo(view, with: slideInMenuPadding)
         containerView.edgeTo(view)
-        //view.addSubview(tableView)
+        containerView.isHidden = true
+        menuView.pinMenuTo(view, with: (view.frame.width - slideInMenuPadding))
     }
 }
 
-public extension UIView {
+extension UIView {
+    
     func edgeTo(_ view: UIView) {
         view.addSubview(self)
         translatesAutoresizingMaskIntoConstraints = false
@@ -80,8 +119,8 @@ public extension UIView {
         view.addSubview(self)
         translatesAutoresizingMaskIntoConstraints = false
         topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-        leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -constant).isActive = true
+        widthAnchor.constraint(equalToConstant: constant).isActive = true
+        trailingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
     }
 }
